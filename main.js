661ebe7026e18d61ff418ee707e83819f9105aa5
gogoringhye <145514996+gogoringhye@users.noreply.github.com>
2025-03-19 01:00:56 +0900

    Update main.js

--- main.js
+++ main.js
@@ -15,7 +15,7 @@ if (cookieCheck > -1) {
 
 checkboxElement.addEventListener('change', () => {
   let date = new Date(); //오늘 날짜
-  date.setDate(date.getDate() + 7) //만료일을 만듦
+  date.setDate(date.getDate() + 7) //만료일 만듦
   //console.log(date)
 
   if (checkboxElement.checked) { //input에 check가 되었다면
@@ -753,3 +753,79 @@ document.addEventListener("DOMContentLoaded", function () {
   });
 });
 /* //탑버튼 */
+
+
+/* 타이핑 */
+// get the element
+const text = document.querySelector('.typing-text');
+
+// make a words array
+const words = [
+  "build.",
+  "design.",
+  "refine.",
+  "optimize."
+];
+
+// start typing effect
+setTyper(text, words);
+
+function setTyper(element, words) {
+
+  const LETTER_TYPE_DELAY = 75;
+  const WORD_STAY_DELAY = 2000;
+
+  const DIRECTION_FORWARDS = 0;
+  const DIRECTION_BACKWARDS = 1;
+
+  var direction = DIRECTION_FORWARDS;
+  var wordIndex = 0;
+  var letterIndex = 0;
+
+  var wordTypeInterval;
+
+  startTyping();
+
+  function startTyping() {
+    wordTypeInterval = setInterval(typeLetter, LETTER_TYPE_DELAY);
+  }
+
+  function typeLetter() {
+    const word = words[wordIndex];
+
+    if (direction == DIRECTION_FORWARDS) {
+      letterIndex++;
+
+      if (letterIndex == word.length) {
+        direction = DIRECTION_BACKWARDS;
+        clearInterval(wordTypeInterval);
+        setTimeout(startTyping, WORD_STAY_DELAY);
+      }
+
+    } else if (direction == DIRECTION_BACKWARDS) {
+      letterIndex--;
+
+      if (letterIndex == 0) {
+        nextWord();
+      }
+    }
+
+    const textToType = word.substring(0, letterIndex);
+
+    element.textContent = textToType;
+  }
+
+  function nextWord() {
+
+    letterIndex = 0;
+    direction = DIRECTION_FORWARDS;
+    wordIndex++;
+
+    if (wordIndex == words.length) {
+      wordIndex = 0;
+    }
+
+  }
+}
+
+/* //타이핑 */
